722e1fe876566aac2d54a1604bc5a72097e48e2a
guolinhao <gavin141113@163.com>
2020-12-27 00:54:12 +0800

    [Feat]: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button> hello </Button>
+      <Button disabled> disabled </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small</Button>
+      <Button btnType={ButtonType.Link} href="www.baidu.com">Baidu</Button>
+      <Button btnType={ButtonType.Link} href="www.baidu.com" disabled>disabled Baidu</Button>
     </div>
   );
 }
